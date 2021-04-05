e2c64a5f6cba3c99224a6d3da084b61e017be13b
Sumit Mondal <53532220+SumitMondal26@users.noreply.github.com>
2021-04-05 13:52:10 +0530

    Update settings.py

--- shopping_project/settings.py
+++ shopping_project/settings.py
@@ -127,12 +127,13 @@ USE_TZ = True
 
 import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), ) 
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-
 MEDIA_URL='/media/'
